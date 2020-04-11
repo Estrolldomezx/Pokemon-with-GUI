c6760fbf41b7d4c29cb2836fa1074274f28a17cf
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-11 20:46:02 +0700

    Add files via upload

---
+++ GUIstage.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.TimeUnit;
+/**
+ *
+ * @author Admin
+ */
+public class GUIstage extends JFrame{
+    private SendMons allMon  = new SendMons();
+    private Pokemon m1 = allMon.outMon();
+    private Pokemon m2 = allMon.outMon();
+    private Pokemon m3 = allMon.outMon();
+    
+}
