90b6388466dce87a95752358ca1695734dc1725f
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-11 16:40:03 +0700

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Item {
+    private String itemName;
+    
+    public Item(String iname){
+        this.itemName = iname;
+    }
+    public  String getName(){
+        return itemName;
+    }
+    public String itemtoString(){
+        return itemName;
+    }
+    
+}
