7d6650913401cb7a42d141c38b554d215131b334
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-11 18:45:10 +0700

    Add files via upload

---
+++ Skills.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Skills {
+    private String skillName;
+    private int skillDamage;
+    private int skillHeal;
+    private int skillwithMana;
+    public Skills(String sname, int sdamage, int sheal, int swithmana){
+        this.skillDamage = sdamage;
+        this.skillHeal = sheal;
+        this.skillName = sname;
+        this.skillwithMana = swithmana;
+    }
+    public String getName(){
+        return skillName;
+    }
+    public int getDamge(){
+        return skillDamage;
+    }
+    public int getheal(){
+        return skillHeal;
+    }
+    public int getMana(){
+        return skillwithMana;
+    }
+    
+}
