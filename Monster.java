5d13f583454f608bacee9dad07a7c2a8b26c7294
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-11 23:07:03 +0700

    Add files via upload

---
+++ Monster.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Monster extends BasePokemon{
+    private int monDamage;
+    
+    public Monster(String monname, int monhp, int damage){
+        super(monname);
+        this.CharHP = monhp;
+        this.monDamage = damage;
+    }
+    
+    public int getMonsterDamage(){
+        return monDamage;
+    }
+}
