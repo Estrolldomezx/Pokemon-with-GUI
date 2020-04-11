49b631f83944cece1f119b5a73d8a776ab7685da
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-11 16:15:47 +0700

    Update Base.java

--- Base.java
+++ Base.java
@@ -9,5 +9,79 @@
  * @author Admin
  */
 public class Base {
+    private int mana;
+    private int def; //armor
+    private int exp;
+    private int level;
+    private int damage;
+    private int pokeball; // money
+    private int fight; //killmonster
+    private String skill;
+    private int chargeATK; // MaxMana
+    private int maxEXP;
+    private Bag myBag; //Nbag
     
+    public Base(String baseName, String baseSkill, int baseDamage, int baseHP, int baseEXP, int basePokeball, int baseMana, int baseDef, int baseLevel){
+        super(baseName);
+        this.charHP = baseHP;
+        this.mana =baseMana;
+        this.def = baseDef;
+        this.exp = baseEXP;
+        this.pokeball = basePokeball;
+        this.level = baseLevel;
+        this.damage = baseDamage;
+        this.skill = baseSkill;
+        this.fight = baseKillmon;
+        this.MaxCharHP = 100;
+        this.chargeATK = 0;
+        this.maxEXP = 0;
+        this.myBag = new Bag();
+        
+        }
+        public int getMana(){
+            return mana;
+        }
+        public int getDEF(){
+            return def;
+        }
+        public int getEXP(){
+            return exp;
+        }
+        public int getLevel(){
+            return level;
+        }
+        public int getDamage(){
+            return damage;
+        }
+        public int getKillmon(){
+            return fight;
+        }
+        public int getPokeball(){
+            return pokeball;
+        }
+        public String getBaseSkill(){
+            return skill;
+        }
+        public void getItem(Item item){
+            myBag.AddItem(item);
+        }
+        public boolean getLevel(){
+            if(exp == maxEXP){
+                level++;
+                return true;
+            }
+            else 
+                return false;
+        }
+        public void getEXP(int newEXP){
+            exp += newEXP;
+            getLevel();
+        }
+        
+        
 }
+        
+        
+        
+    
+    
