4a0ef0a197cd6fff161185f6805871cbe94b4e7e
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-15 17:00:59 +0700

    Update Allitems.java

--- Allitems.java
+++ Allitems.java
@@ -10,25 +10,30 @@ import java.util.Random;
  * @author Admin
  */
 public class Allitems{
-    private ArrayList<Item> AllItems;
+    private ArrayList<Item> AllItem1; // edit push final
     public Healing item1 = new Healing("Berry", 30, 10);
     public Healing item2 = new Healing("Potion", 40, 12);
     public Healing item3 = new Healing("Super Potion", 50, 15);
-    public Pokeball item4 = new Pokeball("Pokeball",150);
+    public Pokeball item4 = new Pokeball("Pokeball", 150);
+
     Random rand = new Random();
-    public Allitems(){
-        this.AllItems = new ArrayList();
-        AllItems.add(item1);
-        AllItems.add(item2);
-        AllItems.add(item3);
-        AllItems.add(item4);
+
+    public Allitems() {
+        this.AllItem1 = new ArrayList();
+        AllItem1.add(item1);
+        AllItem1.add(item2);
+        AllItem1.add(item3);
+        AllItem1.add(item4);
+    }
+
+    public Item dropItem() {
+        int i;
+        i = rand.nextInt(4);
+        return AllItem1.get(i);
+    }
+
+    public Item getItem(int i) {
+        return AllItem1.get(i);
     }
     
-    public Item dropItem(){
-        int i = rand.nextInt(10);
-        return AllItems.get(i);
-    }
-    public Item getItem(int i){
-        return AllItems.get(i);
-    }
 }
