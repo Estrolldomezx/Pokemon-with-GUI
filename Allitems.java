f48f93b83c6227368fc8374a09d5198f0cf8a2d1
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-19 13:03:49 +0700

    Update Allitems.java

--- Allitems.java
+++ Allitems.java
@@ -15,24 +15,30 @@ public class Allitems{
     public Healing item2 = new Healing("Potion", 40, 12);
     public Healing item3 = new Healing("Super Potion", 50, 15);
     public Pokeball item4 = new Pokeball("Pokeball", 150);
-
     Random rand = new Random();
 
     public Allitems() {
-        this.AllItem1 = new ArrayList();
+        this.AllItem1 = new ArrayList<Item>();
         AllItem1.add(item1);
         AllItem1.add(item2);
         AllItem1.add(item3);
         AllItem1.add(item4);
     }
 
-    public Item dropItem() {
+    public Item dropItem(){
         int i;
-        i = rand.nextInt(4);
-        return AllItem1.get(i);
+        //i = rand.nextInt(3);
+        i = Math.random()*3;
+        try{
+            if(i == 0 || i == 1 || i == 2 || i ==3)
+                return Allitem1.get(i);
+        }    
+        catch(IndexOutOfBoundsException e){
+            System.out.printf("Error random");
+        }
     }
 
-    public Item getItem(int i) {
+    public Item getItem(int i){
         return AllItem1.get(i);
     }
     
