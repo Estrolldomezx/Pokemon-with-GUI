ba686eee5ab6287d587c327659e33711dee89678
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-27 05:03:09 +0700

    Update Pokename.java

--- Pokename.java
+++ Pokename.java
@@ -1,5 +1,7 @@
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Desktop.Action;
 import java.awt.event.ActionEvent;
@@ -13,6 +15,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout.Constraints;
 
+import jdk.internal.org.jline.utils.Colors;
+
 public class Pokename extends JFrame{
     JTextField nametext;
     String name;
@@ -20,19 +24,30 @@ public class Pokename extends JFrame{
     public Pokename(){
         super("Become a Pokemon trainer");
         Container container = getContentPane();
-        container.setLayout(new GridLayout(2, 1));
 
         //p1
         JPanel p1 =new JPanel();
-        ImageIcon iconlogo =new ImageIcon("Logo.png");
+        ImageIcon iconlogo =new ImageIcon("img\\bg2.GIF");
         JLabel logo = new JLabel(iconlogo);
+        logo.setBounds(50, 0, 250, 250);
 
+        JLabel nameLabel = new JLabel("Enter Your Buddy Name");
+        nameLabel.setFont(new Font("Eras Demi ITC", Font.BOLD, 16));
+        JTextField nametext = new JTextField(20);
+        nametext.setFont(new Font("Eras Demi ITC", Font.BOLD, 15));
+        JButton setNameButton = new JButton(" SUBMIT ");
+        setNameButton.setFont(new Font("Eras Demi ITC", Font.BOLD, 14));
         p1.add(logo);
+        //p1.add(nullLabel);
+        p1.add(nameLabel);
+        p1.add(nametext);
+        p1.add(setNameButton);
         p1.setLayout(new FlowLayout());
+        p1.setBackground(new Color(255, 215, 0));
         container.add(p1);
 
         //p2
-        JPanel p2 = new JPanel();
+/*        JPanel p2 = new JPanel();
         p2.setLayout(new FlowLayout());
         JLabel nameLabel = new JLabel("Enter Your Name");
         p2.add(nameLabel);
@@ -40,12 +55,35 @@ public class Pokename extends JFrame{
         p2.add(nametext);
         JButton setNameButton = new JButton("Submit your buddy name");
         p2.add(setNameButton);
+        p2.setBounds(0, 250, 500, 250);
 
         container.add(p1);
         container.add(p2);
+*/        
+        setNameButton.addActionListener(new ActionListener(){
+        
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                name = nametext.getText();
 
-        pack();
+                new GUIChoosePokemon(name);
+                dispose();
+            }
+        });
+        nametext.addActionListener(new ActionListener(){
+        
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                name = nametext.getText();
+                
+                new GUIChoosePokemon(name);
+                dispose();
+            }
+        });
+        //pack();
+        setSize(350, 400);
         setVisible(true);
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
