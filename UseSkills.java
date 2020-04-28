f1d9de86f3b49950bb9539c1f2d6bddccb11daf2
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-29 05:21:53 +0700

    Update UseSkills.java

--- UseSkills.java
+++ UseSkills.java
@@ -15,10 +15,10 @@ public class UseSkills {
     private Skills skill4;
     
     public UseSkills(){
-        this.skill1 = new Skills("Solar beam", 40, 0, 80);
-        this.skill2 = new Skills("Fire blast", 70, 0, 90);
-        this.skill3 = new Skills("Hydro pump", 30, 0 , 50);
-        this.skill4 = new Skills("Thunder bolt", 60, 0, 50);
+        this.skill1 = new Skills("Solar beam", 40, 10, 80);
+        this.skill2 = new Skills("Fire blast", 70, 10, 90);
+        this.skill3 = new Skills("Hydro pump", 30, 10 , 50);
+        this.skill4 = new Skills("Thunder bolt", 60, 10, 50);
     }
     public Skills getBBSSkill(){
         return skill1;
