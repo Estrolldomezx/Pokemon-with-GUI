e742cce1fd73e25195c34bdf990b1e9c5858c768
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-29 05:00:55 +0700

    Update Allitems.java

--- Allitems.java
+++ Allitems.java
@@ -3,62 +3,74 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.util.ArrayList;
-import java.util.Random;
+
 /**
  *
  * @author Admin
  */
-public class Allitems{
-    private ArrayList<Item> AllItem1; // edit push final
-    public Healing item1 = new Healing("Berry", 30, 10);
-    public Healing item2 = new Healing("Potion", 40, 12);
-    public Healing item3 = new Healing("Super Potion", 50, 15);
-    public Stone item4 = new Stone("Upgrade", 10);
-    public Stone item5 = new Stone("Dragon Scale", 20);
-    public Stone item6 = new Stone("King Rock", 30);
-    public Stone item7 = new Stone("Metal Coat",40);
-    public Stone item8 = new Stone("Sinnoh Stone",50);
-    public Stone item9 = new Stone("Sun Stone",50);
-    public Stone item10 = new Stone("Unova Stone",60); 
+public class AllPokemons {
+    private Mons1 p1;
+    private Mons1 p11;
+    private Mons1 p111;
+    private Mons1 p2;
+    private Mons1 p22;
+    private Mons1 p222;
+    private Mons1 p3;
+    private Mons1 p33;
+    private Mons1 p333;
+    private Mons1 p4;
+    private Mons1 p44;
 
-    public Pokeball item11 = new Pokeball("Pokeball", 150);
-    Random rand = new Random();
-    int i = (int)Math.random()*10;
-    
-    public Allitems() {
-        this.AllItem1 = new ArrayList();
-        AllItem1.add(item1);
-        AllItem1.add(item2);
-        AllItem1.add(item3);
-        AllItem1.add(item4);
-        AllItem1.add(item5);
-        AllItem1.add(item6);
-        AllItem1.add(item7);
-        AllItem1.add(item8);
-        AllItem1.add(item9);
-        AllItem1.add(item10);
+  
+    public AllPokemons(){
+        this.p1 = new Mons1("Bulbasaur","Solar beam",100);
+        this.p11 = new Mons1("Bulbasaur2","Solar beam",100);
+        this.p111 = new Mons1("Bulbasaur3","Solar beam",100);
+        this.p2 = new Mons1("Charmander","Fire blast",100);
+        this.p22 = new Mons1("Charmander2","Fire blast",100);
+        this.p222 = new Mons1("Charmander3","Fire blast",100);
+        this.p3 = new Mons1("Squirtle","Hydro pump",100);
+        this.p33 = new Mons1("Squirtle2","Hydro pump",100);
+        this.p333 = new Mons1("Squirtle3","Hydro pump",100);
+        this.p4 = new Mons1("Pikachu","Thunder bolt",100);
+        this.p44 = new Mons1("Pikachu2","Thunder bolt",100);
+
+    } 
+    public Mons1 getBBS(){
+        return p1;
     }
-
-    public Item dropItem(){
-        try{
-            return AllItem1.get(i);
-        }
-        catch(NullPointerException ex1){
-            System.out.print("Exception occured :" + ex1);
-        }
-        return AllItem1.get(i);
-
+    public Mons1 getBBS2(){
+        return p11;
     }
-
-    public Item getItem(int i){
-        try{
-            return AllItem1.get(i);
-        }
-        catch(IndexOutOfBoundsException ex2){
-            System.out.print("Exception occured :" + ex2);
-        }
-        return AllItem1.get(i);
+    public Mons1 getBBS3(){
+        return p111;
     }
     
+    public Mons1 getCMD(){
+        return p2;
+    }
+    public Mons1 getCMD2(){
+        return p22;
+    }
+    public Mons1 getCMD3(){
+        return p222;
+    }
+    public Mons1 getSQT(){
+        return p3;
+    }
+    public Mons1 getSQT2(){
+        return p33;
+    }
+    public Mons1 getSQT3(){
+        return p333;
+    }
+    
+    public Mons1 getPKC(){
+        return p4;
+    }
+    public Mons1 getPKC2(){
+        return p44;
+    }
+
 }
+        
