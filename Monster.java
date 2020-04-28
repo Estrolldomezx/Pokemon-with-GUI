d440faf252ac9950d64335cd9739da453b7dc7d5
Wanish Sengkhim <52479363+Estrolldomezx@users.noreply.github.com>
2020-04-29 05:15:55 +0700

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -12,7 +12,7 @@ public class Monster extends BasePokemon{
     private int monDamage;
     
     public Monster(String monname, int monhp, int damage){
-        super(monname);
+        super(monDamage);
         this.CharHP = monhp;
         this.monDamage = damage;
     }
